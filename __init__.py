729a6e5d77e92aaddeafdad3cc419847c5d6b8fd
AndrewPras <42874830+AndrewPras@users.noreply.github.com>
2018-11-15 21:44:59 -0500

    Create __init__.py
    -Initialize FPS object
    -Record gray scale screen, returning frame count
    -print elapsed time of FPS object, and actual realized FPS

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from ScreenCapture import screen_record_efficient
+from FPS import FPS
+
+
+frames = FPS().start()
+fps_count = screen_record_efficient()
+
+print("MSS:", str(fps_count))
+frames.stop()
+print('[INFO] elapsed time: {:.2f}'.format(frames.elapsed()))
+print('[INFO] approx. FPS: {:.2f}'.format(fps_count / frames.elapsed()))
